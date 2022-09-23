483a4c7f2a4358a11910109ac5bdd731f733a1e0
Peter charles gat-eb <pcharlesgateb@gmail.com>
2022-09-23 14:33:31 +0800

    first commit

--- phpexercise1.php
+++ phpexercise1.php
@@ -1,18 +1,23 @@
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        echo "Twinkle, Twinkle little Star! </br> ";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php 
+   
+echo "Twinkle, Twinkle little Star! </br> ";
         
         $twinkle = "Twinkle";
         $star ="Star";
         
-        echo " $twinkle, $twinkle little $star! </br> ";
-        
-        ?>
-    </body>
+echo " $twinkle, $twinkle little $star! </br> ";
+      
+ ?>
+    
+    
+</body>
 </html>
