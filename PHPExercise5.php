7e11a2c3232364cd939e6b66e72f48ee65ebce8e
Peter charles gat-eb <pcharlesgateb@gmail.com>
2022-09-23 14:29:40 +0800

    first commit

---
+++ PHPExercise5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+
+<?php  
+ 
+ $around = "around"; 
+echo'What goes ' . $around . ' comes ' . $around . '.';
+
+?> 
+		
+</body>
+</html>
---
+++ PHPExercise6.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+for ($count = 1; $count < 13; $count++){
+  $squared = $count * $count;
+  
+  print("$count squared is $squared <br>");
+  
+}
+
+?>
+
+	
+	
+</body>
+</html>
